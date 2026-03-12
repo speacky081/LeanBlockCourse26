e54974fc5c1864ba95d848cbc81d0a8145f89da3
Christoph Spiegel <christophspiegel.berlin@gmail.com>
2026-03-12 10:38:36 +0100

    added some info on Nat.Prime, Set.Finite, and Finset

--- LeanBlockCourse26/P06_Mathlib/S01_PrimeTheorem.lean
+++ LeanBlockCourse26/P06_Mathlib/S01_PrimeTheorem.lean
@@ -47,39 +47,138 @@ To do this, you should:
       use [leansearch.net](https://leansearch.net) or [Loogle](https://loogle.lean-lang.org),
       talk to people on [Is there code for X? on zulip](https://leanprover.zulipchat.com/#narrow/channel/217875-Is-there-code-for-X.3F/topic/Complexity.20theory/with/578655619)
       or ask ChatGPT, Claude, ... with online research tools and maybe even a lean environment.
+
+**When is a `Nat` `Prime`?**
+
+`Mathlib.Data.Nat.Prime.Defs` tells us that `def Prime (p : ℕ) := Irreducible`,
+which takes us to `Mathlib.Algebra.Groups.Irreducible.Defs`... but really you
+probably just want to use one of the many more intuitive definitions and
+properties stated in `Mathlib.Data.Nat.Prime.Defs`, for example:
+
+```
+variable {p : ℕ}
+
+theorem prime_def : Prime p ↔ 2 ≤ p ∧ ∀ m, m ∣ p → m = 1 ∨ m = p := ...
+
+theorem prime_def_lt : Prime p ↔ 2 ≤ p ∧ ∀ m < p, m ∣ p → m = 1 := ...
+
+theorem prime_def_lt' : Prime p ↔ 2 ≤ p ∧ ∀ m, 2 ≤ m → m < p → ¬m ∣ p := ...
+
+theorem prime_iff_not_exists_mul_eq :
+   p.Prime ↔ 2 ≤ p ∧ ¬ ∃ m n, m < p ∧ n < p ∧ m * n = p := ...
+```
+
+### When is a `Set` `Finite` ...
+
+We have already seen in P03 that `Set` is just defined as `Type → Type`.
+The relevant part of mathlib to start with is `import Mathlib.Data.Set.Basic`.
+For the notion of when a `Set` is `Finite` or `Infinite`, one starting
+point in mathlib is `Mathlib.Data.Finite.Defs`. There we see that ...
+
+```
+Infinite (s : Set α) : Prop := ¬s.Finite`
+```
+
+... also captured by ...
+
+```
+theorem not_infinite {s : Set α} : ¬s.Infinite ↔ s.Finite := sorry
+```
+
+... and `Finite` itself is an inductively defined type class ...
+
+```
+class inductive Finite (α : Sort*) : Prop
+   | intro {n : ℕ} : α ≃ Fin n → Finite _
+```
+
+... based on the type `Fin n`, capturing `Nat` that are strictly less
+than `n`, defined in `Lean.Init.Prelude` as a structure ...
+
+```
+structure Fin (n : Nat) where
+   mk ::
+   val  : Nat
+   isLt : LT.lt val n
+```
+
+### ... and how does it differ from `Finset`?
+
+In `Mathlib.Data.Finset.Def` we can find the  
+
+```
+structure Finset (α : Type*) where
+  val : Multiset α
+  nodup : Nodup val
+```
+
+`Mathlib.Data.Set.Finite.Basic` tells you that for any pair of
+infinite and finite set we can find an element only in the former ...
+
+```
+theorem Set.Infinite.exists_notMem_finset (hs : s.Infinite) (t : Finset α) :
+   ∃ a ∈ s, a ∉ t := ...
+```
+
+... as well as coerce any `Finite` `Set` to a `Finset`...
+
+```
+def Set.Finite.toFinset {s : Set α} (h : s.Finite) : Finset α :=
+   @Set.toFinset _ _ h.fintype
+```
+
+... while using the fact that an element needs to exist in both versions ...
+
+```
+theorem Set.mem_toFinset : a ∈ hs.toFinset ↔ a ∈ s := ...
+```  
 -/
 
+theorem infinitude_of_primes_tfae {ℵ₀} : [
+   /-
+   **(1) The set of primes is infinite**
+   -/
+   { p : ℕ | p.Prime }.Infinite,
 
-#check Fintype
+   /-
+   **(2) The subtype of primes is infinite**
+   -/
+   Infinite { p : ℕ // p.Prime },
 
-theorem infinitude_of_primes_tfae : [
+   /-
+   **(3) For any finite set we can find a prime number outside of it**
+   -/
+   ∀ (S : Finset ℕ), (∃ p ∉ S, p.Prime),
 
-    -- 1) The set of primes is infinite
-    { p : ℕ | p.Prime }.Infinite,
+   /-
+   **(4) For any finite set *of primes* we can find a prime outside of it**
+   -/
+   (∀ (S : Finset ℕ) (_ : ∀ s ∈ S, Nat.Prime s), (∃ p ∉ S, p.Prime)),
 
-    -- 2) The subtype of primes is infinite
-    Infinite { p : ℕ // p.Prime },
+   /-
+   **(5) For any natural number there exists a prime strictly greater than it**
+   -/
+   (∀ n : ℕ, (∃ p > n, p.Prime)),
 
-    -- 3) For any finite set we can find a prime number outside of it
-    ∀ (S : Finset ℕ), (∃ p ∉ S, p.Prime),
+   /-
+   **(6) There exists an injection from the Natural numbers into the primes**
+   -/
+   ∃ (P : ℕ → ℕ) (h : P.Injective), (∀ k, (P k).Prime),
 
-    -- 4) For any finite set *of prime numbers* we can find a prime number outside of it
-    (∀ (S : Finset ℕ) (_ : ∀ s ∈ S, Nat.Prime s), (∃ p ∉ S, p.Prime)),
+   /-
+   **(7) The sequence of primes is strictly monotone increasing**
+   -/
+   StrictMono (Nat.nth Nat.Prime),
 
-    -- 5) For any natural number there exists a prime strictly greater than it
-    (∀ n : ℕ, (∃ p > n, p.Prime)),
+   /-
+   **(8) The prime counting function is unbounded**
+   -/
+   ∀ n : ℕ, ∃ m, n ≤ Nat.primeCounting m,
 
-    -- 6) There exists an injection from the Natural numbers into the primes
-    ∃ (P : ℕ → ℕ) (h : P.Injective), (∀ k, (P k).Prime),
-
-    -- 7) The sequence of primes is strictly monotone increasing
-    StrictMono (Nat.nth Nat.Prime),
-
-    -- 8) The prime counting function is unbounded
-    ∀ n : ℕ, ∃ m, n ≤ Nat.primeCounting m,
-
-    -- 9) The cardinality of the primes equals ℵ₀
-    Cardinal.mk { p : ℕ // p.Prime } = ℵ₀,
+   /-
+   **(9) The cardinality of the primes equals ℵ₀**
+   -/
+   Cardinal.mk { p : ℕ // p.Prime } = ℵ₀,
   ].TFAE := by
 
   tfae_have 5 → 6 := by sorry -- Theo
